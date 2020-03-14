87d93b5cc5a84f97b86c7d50de32a7b9e7f9637b
vex-xcc <vex.xcc@gmail.com>
2020-03-14 18:14:17 +0300

    add login route to log in the employee with error to fix in the Authentication

--- EnjazBackEnd/Routes/ emp.js
+++ EnjazBackEnd/Routes/ emp.js
@@ -35,3 +35,48 @@ router.post('/register', (req, res) => {
         }
     });
 });
+
+
+router.post('/login', (req, res, next) => {
+    const empUsername = req.body.empUsername;
+    const password = req.body.password;
+
+    Emp.getUserByUsername(empUsername, (err, user) => {
+        if (err) throw err;
+        if (!user) {
+            return res.json({
+                success: false,
+                message: "Emp not found."
+            });
+        }
+
+        Emp.comparePassword(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+                const token = jwt.sign({
+                    type: "user",
+                    data: {
+                        _id: user._id,
+                        empUsername: user.empUsername,
+                        empFullName: user.empFullName,
+                        email: user.email,
+                        empPhone: user.empPhone
+                    }
+                }, config.secret, {
+                    expiresIn: 36000000 // for 1 week time in milliseconds
+                });
+                return res.json({
+                    success: true,
+                    token: "jwt " + token
+                });
+            } else {
+                return res.json({
+                    success: true,
+                    message: "Wrong Password."
+                });
+            }
+        });
+    });
+});
+
+module.exports = router;
