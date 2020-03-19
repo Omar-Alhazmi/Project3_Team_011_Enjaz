4bdf29f8ce38da6bbea4f1c8398e601e4449a66b
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-03-19 14:38:14 +0300

    AdminHeader

---
+++ enjaz-front-end/src/components/Header/AdminHeader.js
@@ -0,0 +1,85 @@
+//AdminHeader
+import React from "react";
+import "./header.css";
+import Tickets from "../Ticket/Tickets";
+import { getAllTicket } from "../api";
+import { getInfo } from '../login/decodeToken'
+export default class AdminHeader extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: null,
+            Fltir: "none",
+            FltirTicket: "display"
+        };
+    }
+    componentDidMount() {
+        // Mack API call
+        getAllTicket()
+            .then(reponse => {
+                console.log("reponse.data", reponse.data);
+                const history = reponse.data.filter(Ticket => {
+                    if (Ticket.TicketState === "closed") {
+                        return reponse.data;
+                    }
+                });
+                this.setState({ history });
+            })
+            .catch(error => {
+                console.log(" API error: ", error);
+            });
+    }
+    TicketClicked = (e) => {
+        if (this.state.Fltir === 'none') {
+            this.setState({
+                Fltir: 'display',
+                FltirTicket: 'none'
+            });
+        }
+        else {
+            this.setState({
+                Fltir: 'none',
+                FltirTicket: 'display'
+            });
+        }
+    }
+
+    render() {
+        console.log(getInfo())
+        return (
+            <div className="page">
+                <header tabindex="0">Enjaz</header>
+                <div className="nav-container">
+                    <div className="bg"></div>
+                    <div className="button" tabindex="0">
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                        <span className="icon-bar"></span>
+                    </div>
+
+                    <div className="nav-content" tabindex="0">
+                        <ul>
+                            <li onClick={this.TicketClicked}>NewEmployee</li>
+
+                            <li>
+                                <a href="">History</a>
+                            </li>
+                            <li>
+                                <a onClick={e => this.logOut(e)}>Log out </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <main>
+                    <div className="content">
+                        <div className={`Tickets-${this.state.FltirTicket}`}>
+                            <Tickets />
+                        </div>
+                        <div className={`NewEmployee-${this.state.Fltir}`}>
+                        </div>
+                    </div>
+                </main>
+            </div>
+        );
+    }
+}
