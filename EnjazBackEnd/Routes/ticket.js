a92983af07b6191d5d5b09de709842651a67c446
vex-xcc <vex.xcc@gmail.com>
2020-03-19 11:23:42 +0300

    add fix ticket router create ticket

--- EnjazBackEnd/Routes/ticket.js
+++ EnjazBackEnd/Routes/ticket.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
 const Emp = require('../models/Emp');
 const Ticket = require('../models/Ticket');
-
-
-
 //-------------create ticket embedded in emp-------------------
 router.post('/:empId', (req, res) => {
   // store new ticket in memory with data from request body
@@ -14,13 +10,13 @@ router.post('/:empId', (req, res) => {
   ticket.TicketType = TicketType,
     ticket.TicketDescription = TicketDescription,
     ticket.TicketState = TicketState,
-    ticket.TicketsEmp = [req.params.empId]
+    ticket.sendTickets = req.params.empId
   const savedTicket = new Ticket(ticket)
   savedTicket.save()
   // find emp in db by id and add new ticket
   Emp.findById(req.params.empId, async (error, foundEmp) => {
     try {
-      await foundEmp.Tickets.push(savedTicket);
+      await foundEmp.sendTickets.push(savedTicket);
       foundEmp.save()
       res.status(200).json(savedTicket);
     }
