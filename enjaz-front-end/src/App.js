1f9c505244f816e686349bb1c20a209ec6f5a57e
vex-xcc <vex.xcc@gmail.com>
2020-03-19 11:32:55 +0300

    add app and protect AuthComponent

--- enjaz-front-end/src/App.js
+++ enjaz-front-end/src/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react';
+import apiURL from'./APIconfig';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
+// Components
+import Login from './components/login/Login'
+import AdminHeader from './components/Header/AdminHeader'
+import EmpHeader from './components/Header/EmpHeader'
+import AuthComponent from './components/login/AuthenticatedComponent';
 
-
-
-export default class App extends Component {
-  render() {
+export default class App extends React.Component {
+  render(){
+    console.log('MY API :  ',apiURL);
     return (
       <>
-        
+      <BrowserRouter>
+     <Switch>
+      <Route path={'/login'} exact component={Login}/>   
+    <AuthComponent>
+      <Route path={'/EmpHeader'} component={EmpHeader}/>
+      <Route path={'/AdminHeader'} component={AdminHeader}/>
+      </AuthComponent>
+        </Switch>
+      </BrowserRouter>
       </>
-    )
+    );
   }
 }
