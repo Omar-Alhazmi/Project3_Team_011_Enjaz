c7c6d4123225c7820ec7230a8031ee18da0d37ae
vex-xcc <vex.xcc@gmail.com>
2020-03-21 17:11:36 +0300

    add

--- server.js
+++ server.js
@@ -90,9 +90,9 @@ if (process.env.NODE_ENV === 'production') {
 
 // app.use(checkUserType);
 
-app.use(empRoute);
+app.use('/api/emp',empRoute);
 // app.use("/api/admin", adminRoute);
-app.use(ticketRoute);
+app.use('/api',ticketRoute);
 // Handler for 404 - Resource Not Found
 app.use((req, res, next) => {
   res.status(404).send("We think you are lost!");
