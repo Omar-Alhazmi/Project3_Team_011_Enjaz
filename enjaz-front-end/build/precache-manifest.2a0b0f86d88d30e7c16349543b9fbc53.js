ee0e095bb0a93703cdb1db7cb48d78a270650ae2
vex-xcc <vex.xcc@gmail.com>
2020-03-27 20:35:29 +0300

    add build

---
+++ enjaz-front-end/build/precache-manifest.2a0b0f86d88d30e7c16349543b9fbc53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3852f11ebc71f97c6e5e92d9e135c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "421e4e7b5b64d97181a9",
+    "url": "/static/css/main.ad76807d.chunk.css"
+  },
+  {
+    "revision": "09ff269a8760b81c70cd",
+    "url": "/static/js/2.9267ddc6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9267ddc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "421e4e7b5b64d97181a9",
+    "url": "/static/js/main.d383fd22.chunk.js"
+  },
+  {
+    "revision": "cf05db06c7ad55c8a672",
+    "url": "/static/js/runtime-main.a5944c79.js"
+  }
+]);
